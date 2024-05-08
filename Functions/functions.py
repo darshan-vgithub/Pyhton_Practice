a79426af7294c5e6371f71cf36bc6dd0b39a4711
Darshan.V <darshanv487@gmail.com>
2024-05-08 18:26:48 +0530

    learnt importing modules

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,8 +1,19 @@
 def hello_func():
     return "Hello function"
 
-print(hello_func())
-print(hello_func())
-print(hello_func())
-print(hello_func())
-print(hello_func())
+print(hello_func().upper())
+
+
+
+def Greeting(greeting=str,name=str):
+    return "{}, {} how is it learning with python".format(greeting,name)
+
+print(Greeting("Welcome","darshan").upper())
+
+
+def student_info(*args,**kwargs):
+    print(args)
+    print(kwargs)
+courses=["Math","Art"]
+info={"name":"Darshan","age":23}
+student_info(*courses,**info)
---
+++ ImportModules/import.py
@@ -0,0 +1,7 @@
+import index
+
+print(index)
+courses=["History","math","Art","commerce","Mechanical"]
+
+index=index.find_index(courses,"History")
+print(index)
